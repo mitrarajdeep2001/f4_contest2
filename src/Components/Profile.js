95a00b27decdeaa0270f92bace05a64ff0bef576
Rajdeep Mitra <rajdeepmitra8@gmail.com>
2023-06-24 15:24:51 +0530

    Adding Build

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,24 +1,37 @@
-import React from 'react'
+import React from "react";
 
 //Display user details in the profile page
-const Profile = ({data}) => {
+const Profile = ({ data }) => {
   return (
-    <div className='profile-container'>
-        <h1>Profile</h1>
-        <div className='main-container'>
-        <div className='img-container'>
-            <img src={data.image} alt='profile_pic'/>
+    <div className="profile-container">
+      <h1>Profile</h1>
+      <div className="main-container">
+        <div className="img-container">
+          <img src={data.image} alt="profile_pic" />
         </div>
-        <div className='user-info'>
-            <p>Name: <span>{data.firstName} {data.lastName}</span></p>
-            <p>Username: <span>{data.username}</span></p>
-            <p>User ID: <span>{data.id}</span></p>
-            <p>Gender: <span>{data.gender}</span></p>
-            <p>Email: <span>{data.email}</span></p>
+        <div className="user-info">
+          <p>
+            Name:{" "}
+            <span>
+              {data.firstName} {data.lastName}
+            </span>
+          </p>
+          <p>
+            Username: <span>{data.username}</span>
+          </p>
+          <p>
+            User ID: <span>{data.id}</span>
+          </p>
+          <p>
+            Gender: <span>{data.gender}</span>
+          </p>
+          <p>
+            Email: <span>{data.email}</span>
+          </p>
         </div>
-        </div> 
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
